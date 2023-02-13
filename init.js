63faad8b8cf2a4244a51dd1bc015a97d845cdbb0
Classy <69457423+Classy08@users.noreply.github.com>
2023-02-13 07:06:24 +0530

    Fixes to the 100 files limit of readEntries()

--- init.js
+++ init.js
@@ -1,31 +1,42 @@
 let request = chrome.webRequest.onBeforeRequest;
+let file_names = []
 
 chrome.runtime.getPackageDirectoryEntry((root) => {
     let reader = root.createReader();
     reader.readEntries((results) => {
-        searchDir(root, results.filter(x => !['init.js', 'manifest.json', 'README.md', 'LICENSE', '.git', 'charfix.js'].includes(x.name)));
+        searchDir(root, results.filter(x => !['init.js', 'manifest.json', 'README.md', 'LICENSE', '.git'].includes(x.name)));
     });
 });
 
 function searchDir(parent, directories) {
     for (let directory of directories) {
-        parent.getDirectory(directory.name, {create: false}, (dir) => {
-            let reader = dir.createReader();
-
-            reader.readEntries((results) => {
-                let newDirs = results.filter(x => x.isDirectory);
-                let files = results.filter(x => x.isFile);
-                if (newDirs.length) searchDir(dir, newDirs);
-                for (let file of files) {
-                    request.addListener((details) => {
-                        return {
-                            redirectUrl:chrome.extension.getURL(file.fullPath.replace('/crxfs/', ''))
+        parent.getDirectory(directory.name, { create: false }, (dir) => {
+            var reader = dir.createReader();
+            //fix to the 100 folder bottelnecking with create reader function
+            const read = () => {
+                reader.readEntries((results) => {
+                    if (results.length > 0) {
+                        let newDirs = results.filter(x => x.isDirectory);
+                        let files = results.filter(x => x.isFile);
+                        if (newDirs.length) searchDir(dir, newDirs);
+                        for (let file of files) {
+                            request.addListener((details) => {
+                                return {
+                                    redirectUrl: chrome.extension.getURL(file.fullPath.replace('/crxfs/', ''))
+                                }
+                            }, {
+                                urls: [
+                                    '*://*.venge.io/' + file.fullPath.replace('/crxfs/', '') + '*',
+                                ]
+                            }, ['blocking']);
                         }
-                    }, {
-                        urls: ['*://venge.io/' + file.fullPath.replace('/crxfs/', '') + '*']
-                    }, ['blocking']);
-                }
-            });
+                        read()
+                    } else {
+                    }
+                });
+            }
+            read()
         });
     }
 }
+//checks all folders
